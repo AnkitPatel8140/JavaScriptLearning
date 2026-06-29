4d010dd8d632874999d6c75e6cc05aaee37108e2
Ankit399648 <ap806455@gmail.com>
2026-06-29 08:28:53 +0530

    while and do-while loops

---
+++ 13_Loops/02_Loops.js
@@ -0,0 +1,21 @@
+// While loop
+let i = 1;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+
+let arr = ['one', 'two', 'three'];
+let index = 0;
+while(index < arr.length) {
+    console.log(arr[index]);
+    index++;
+}
+
+// do-while loop
+
+let score = 11;
+do {
+    console.log(score);
+    score++;
+} while (score <= 10);
